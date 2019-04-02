d7886e9c7783bf34653cdff65d4c5c29ef1cfe1f
CarMD <carmd@iMac-215-inch-Late-2013.local>
2019-04-02 15:55:39 +0700

    tao trang signin

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBgBaFKT6kv9MYUe9GKyyaTxOZPFbTCZpo",
+    authDomain: "hihihi-42b7b.firebaseapp.com",
+    databaseURL: "https://hihihi-42b7b.firebaseio.com",
+    projectId: "hihihi-42b7b",
+    storageBucket: "hihihi-42b7b.appspot.com",
+    messagingSenderId: "987940131116"
+  }
+};
